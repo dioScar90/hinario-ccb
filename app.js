e79208fa458b0507d9e19e76a1d4fd7902db7f02
Diogo L Scarmagnani <diogols@live.com>
2023-07-01 22:15:50 -0300

    updatedAt

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js'
-import { getFirestore, collection, addDoc, serverTimestamp, doc, deleteDoc, onSnapshot, } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js'
+import { getFirestore, collection, addDoc, updateDoc, serverTimestamp, doc, deleteDoc, onSnapshot, } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDWvbYwZK6CrG1HRrzQzTryYpzsWrpxm-4',
@@ -14,6 +14,9 @@ const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const collectionHymns = collection(db, 'hinos')
 
+const getUrlParam = strGet => new URL(location).searchParams.get(strGet)
+const issetHymnNumber = () => getUrlParam('number')
+
 const formAddHymn = document.querySelector('[data-js="add-hymn-form"]')
 // const hymnsList = document.querySelector('[data-js="hymns-list"]')
 
@@ -23,9 +26,13 @@ const treatFormValues = (form, timestamp = false) => {
   const data = new FormData(form)
   const items = {}
 
-  for (const [key, value] of data) items[key] = value
+  for (const [key, value] of data) {
+    items[key] = key === 'number' ? +value : value
+  }
 
-  if (timestamp === true) items.createdAt = serverTimestamp()
+  if (timestamp === true) {
+    items.createdAt = serverTimestamp()
+  }
 
   return items
 }
@@ -34,9 +41,50 @@ formAddHymn.addEventListener('submit', async function(e) {
   e.preventDefault()
 
   const items = treatFormValues(this, true)
+  const update = items?.update === "true"
+
+  if (update === true) {
+    delete items.update
+    items.updatedAt = serverTimestamp()
+    const doc = await updateDoc(collectionHymns, items)
+    console.log('Document criado com o ID', doc.id)
+
+    this.reset()
+    return
+  }
 
   const doc = await addDoc(collectionHymns, items)
   console.log('Document criado com o ID', doc.id)
 
   this.reset()
 })
+
+const init = () => {
+  const hymnNumber = issetHymnNumber()
+
+  if (hymnNumber) {
+    onSnapshot(collectionHymns, ({ docs }) => {
+      const checkHymnNumber = doc => {
+        return doc._document.data.value.mapValue.fields.number?.integerValue == hymnNumber ||
+        doc._document.data.value.mapValue.fields.number?.stringValue == hymnNumber
+      }
+
+      const rightDoc = docs.find(checkHymnNumber)
+
+      for (const prop in rightDoc) {
+        const input = formAddHymn.querySelector(`input[name="${prop}"]`)
+
+        if (input.tagName === 'SELECT') {
+          [...input.options].forEach(option => option.selected = option.value == rightDoc[prop] ? true : false)
+          continue
+        }
+
+        input.value = rightDoc[prop]
+      }
+
+      formAddHymn.insertAdjacentHTML('afterbegin', `<input type="hidden" name="update" value="true">`)
+    })
+  }
+}
+
+document.addEventListener('DOMContentLoaded', init)
