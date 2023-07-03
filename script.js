38b183540114869eccc702c6efc0f2dbcc7ee911
Diogo L Scarmagnani <diogols@live.com>
2023-07-03 08:32:18 -0300

    update

--- script.js
+++ script.js
@@ -1,137 +1,157 @@
-// import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js'
-// import { getFirestore, collection, query, where, getDocs, addDoc, updateDoc, serverTimestamp, doc, deleteDoc, onSnapshot, }
-//   from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js'
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js'
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  addDoc,
+  updateDoc,
+  serverTimestamp,
+  doc,
+  deleteDoc,
+  onSnapshot,
+} from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js'
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDWvbYwZK6CrG1HRrzQzTryYpzsWrpxm-4',
-//   authDomain: 'hinario-ccb-c94f3.firebaseapp.com',
-//   projectId: 'hinario-ccb-c94f3',
-//   storageBucket: 'hinario-ccb-c94f3.appspot.com',
-//   messagingSenderId: '525179451222',
-//   appId: '1:525179451222:web:645112b1647301d30708f7',
-//   measurementId: 'G-8L1BJ8ZJ68',
-//   // 'Access-Control-Allow-Origin': '*',
-// }
+const firebaseConfig = {
+  apiKey: 'AIzaSyDWvbYwZK6CrG1HRrzQzTryYpzsWrpxm-4',
+  authDomain: 'hinario-ccb-c94f3.firebaseapp.com',
+  projectId: 'hinario-ccb-c94f3',
+  storageBucket: 'hinario-ccb-c94f3.appspot.com',
+  messagingSenderId: '525179451222',
+  appId: '1:525179451222:web:645112b1647301d30708f7',
+  measurementId: 'G-8L1BJ8ZJ68',
+  'Access-Control-Allow-Origin': '*',
+}
 
-// const app = initializeApp(firebaseConfig)
-// const db = getFirestore(app)
-// const collectionHymns = collection(db, 'hinos')
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const collectionHymns = collection(db, 'hinos')
 
-// const getCurrentUrl = () => new URL(location)
-// const getUrlParam = strGet => getCurrentUrl().searchParams.get(strGet)
-// const issetHymnNumber = () => getUrlParam('number')
+const getCurrentUrl = () => new URL(location)
+const getUrlParam = strGet => getCurrentUrl().searchParams.get(strGet)
+const issetHymnNumber = () => getUrlParam('number')
 
-// const formAddHymn = document.querySelector('[data-js="add-hymn-form"]')
-// const btnOpenPdf = document.querySelector('button[data-pdf]')
+const formAddHymn = document.querySelector('[data-js="add-hymn-form"]')
+const btnOpenPdf = document.querySelector('button[data-pdf]')
 
-// onSnapshot(collectionHymns, querySnapshot => {
-//   const { hasPendingWrites } = querySnapshot.metadata
-//   if (!hasPendingWrites)
-//     return
-    
-//   console.log(querySnapshot)
-// })
+onSnapshot(collectionHymns, querySnapshot => {
+  const { hasPendingWrites } = querySnapshot.metadata
+  if (!hasPendingWrites)
+    return
 
-// const parseValue = (key, value) => ({
-//   'number': +value || 0,
-// })[key] || value
+  console.log(querySnapshot)
+})
 
-// const setTimestampValue = items => {
-//   const keyTime = items.id ? 'updatedAt' : 'createdAt'
-//   items[keyTime] = serverTimestamp()
-// }
+const parseValue = (key, value) => ({
+  'number': +value || 0,
+})[key] || value
 
-// const treatFormValues = (form, timestamp = false) => {
-//   const data = new FormData(form)
-//   const items = {}
+const setTimestampValue = items => {
+  const keyTime = items.id ? 'updatedAt' : 'createdAt'
+  items[keyTime] = serverTimestamp()
+}
 
-//   for (const [key, value] of data) {
-//     items[key] = parseValue(key, value)
-//   }
+const treatFormValues = (form, timestamp = false) => {
+  const data = new FormData(form)
+  const items = {}
 
-//   if (timestamp === true) {
-//     setTimestampValue(items)
-//   }
-  
-//   return items
-// }
+  for (const [key, value] of data) {
+    items[key] = parseValue(key, value)
+  }
 
-// function whenFormReset(e) {
-//   const inputId = this.querySelector('input[name="id"]')
-//   const inputNumber = this.querySelector('input[name="number"]')
-//   const btnSubmit = this.querySelector('button[type="submit"]')
+  if (timestamp === true) {
+    setTimestampValue(items)
+  }
 
-//   inputId?.remove()
-//   inputNumber.readOnly = false
-//   btnSubmit.innerText = 'Criar'
-// }
+  return items
+}
 
-// async function whenFormSubmit(e) {
-//   e.preventDefault()
+const whenFormReset = e => {
+  const form = e.target
 
-//   const items = treatFormValues(this, true)
-//   const idToUpdate = items?.id
+  const inputId = form.querySelector('input[name="id"]')
+  const inputNumber = form.querySelector('input[name="number"]')
+  const btnSubmit = form.querySelector('button[type="submit"]')
 
-//   if (idToUpdate) {
-//     delete items.id
-//     const yes = await updateDoc(doc(db, 'hinos', idToUpdate), items)
-//     console.log(yes)
+  inputId?.remove()
+  inputNumber.readOnly = false
+  btnSubmit.innerText = 'Criar'
+}
 
-//     const newUrl = getCurrentUrl()
-//     newUrl.searchParams('number', yes.id)
+const whenFormSubmit = async e => {
+  const form = e.target
 
-//     this.reset()
-//     location = newUrl
-//     return
-//   }
+  e.preventDefault()
 
-//   const { id: createdId } = await addDoc(collectionHymns, items)
-//   console.log('Document criado com o ID', createdId)
+  const items = treatFormValues(form, true)
+  const idToUpdate = items?.id
 
-//   this.reset()
-// }
+  if (idToUpdate) {
+    delete items.id
+    const yes = await updateDoc(doc(db, 'hinos', idToUpdate), items)
+    console.log(yes)
 
-// const renderFormElements = ([ key, value ]) => {
-//   const input = formAddHymn.querySelector(`:is(input,select)[name="${key}"]`)
-  
-//   if (!input) {
-//     if (key === 'id')
-//       formAddHymn.insertAdjacentHTML('afterbegin', `<input type="hidden" name="id" value="${value}">`)
-//     return
-//   }
+    const newUrl = getCurrentUrl()
+    newUrl.searchParams.set('number', yes.id)
 
-//   if (input.tagName === 'SELECT') {
-//     input.options.forEach(option => option.selected = option.value === value ? true : false)
-//     return
-//   }
+    form.reset()
+    location = newUrl
+    return
+  }
 
-//   if (input.name === 'number')
-//     input.readOnly = true
+  const { id: createdId, number } = await addDoc(collectionHymns, items)
+  console.log('Document criado com o ID', items.number)
 
-//   input.value = value
-// }
+  const newUrl = getCurrentUrl()
+  newUrl.searchParams.set('number', items.number)
 
-// const prepareInputsToUpdate = (querySnapshot, hymnNumber) => {
-//   const myDocs = []
-//   querySnapshot.forEach(({ data, id }) => myDocs.push({ ...data(), id }))
-//   const hymn = myDocs.find(({ number }) => number == hymnNumber)
+  form.reset()
+  location = newUrl
+}
 
-//   Object.entries(hymn).forEach(renderFormElements)
+const renderFormElements = ([key, value]) => {
+  const input = formAddHymn.querySelector(`:is(input,select)[name="${key}"]`)
 
-//   formAddHymn.querySelector('button[type="submit"]').innerText = 'Atualizar'
+  if (!input) {
+    if (key === 'id')
+      formAddHymn.insertAdjacentHTML('afterbegin', `<input type="hidden" name="id" value="${value}">`)
+    return
+  }
 
-//   console.log(hymn)
-// }
+  if (input.tagName === 'SELECT') {
+    [...input.options].forEach(option => option.selected = option.value === value ? true : false)
+    return
+  }
 
-// const init = () => {
-//   const hymnNumber = issetHymnNumber()
+  if (input.name === 'number')
+    input.readOnly = true
 
-//   if (hymnNumber) {
-//     onSnapshot(collectionHymns, querySnapshot => prepareInputsToUpdate(querySnapshot, hymnNumber))
-//   }
-// }
+  input.value = value
+}
 
-// formAddHymn.addEventListener('reset', whenFormReset)
-// formAddHymn.addEventListener('submit', whenFormSubmit)
-// btnOpenPdf.addEventListener('click', () => open('hinario_5_organista.pdf', '_blank'))
-// document.addEventListener('DOMContentLoaded', init)
+const prepareInputsToUpdate = (querySnapshot, hymnNumber) => {
+  const myDocs = []
+  querySnapshot.forEach(doc => myDocs.push({ ...doc.data(), id: doc.id }))
+  const hymn = myDocs.find(({ number }) => number == hymnNumber)
+
+  Object.entries(hymn).forEach(renderFormElements)
+
+  formAddHymn.querySelector('button[type="submit"]').innerText = 'Atualizar'
+
+  console.log(hymn)
+}
+
+const init = async () => {
+  const hymnNumber = issetHymnNumber()
+
+  if (hymnNumber) {
+    const querySnapshot = await getDocs(collectionHymns)
+    prepareInputsToUpdate(querySnapshot, hymnNumber)
+  }
+}
+
+formAddHymn.addEventListener('reset', whenFormReset)
+formAddHymn.addEventListener('submit', whenFormSubmit)
+btnOpenPdf.addEventListener('click', () => open('hinario_5_organista.pdf', '_blank'))
+document.addEventListener('DOMContentLoaded', init)
